2807842c18e9b9170442525d47a3554e8e966459
Milan Topuzov <milan.topuzov@salewise.io>
2025-10-06 21:49:29 +0200

    pre-commit: sort imports in rbooking_public_widget.esm.js

--- web_resource_booking/static/src/js/rbooking_public_widget.esm.js
+++ web_resource_booking/static/src/js/rbooking_public_widget.esm.js
@@ -1,7 +1,5 @@
-/** @odoo-module */
-
-import {getTemplate} from "@web/core/templates";
 import {mount} from "@odoo/owl";
+import {getTemplate} from "@web/core/templates";
 import publicWidget from "@web/legacy/js/public/public_widget";
 import RBookingForm from "./rbooking_form.esm";
 
